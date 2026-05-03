71160c23c8c87066206eda08f66f1d590aaf1c5f
AbdelLyon <65748712+AbdelLyon@users.noreply.github.com>
2026-05-04 00:59:57 +0200

    update url

--- src/config/fallback-settings.ts
+++ src/config/fallback-settings.ts
@@ -5,7 +5,7 @@ export const FALLBACK_SITE_SETTINGS = {
   // Informations business
   siteName: 'BNB Énergie 01 - Panneaux Solaires Photovoltaïques Ain',
   businessName: 'BNB Énergie 01',
-  domain: 'https://bnbenergie01.com',
+  domain: 'https://bnbenergie01.com/',
 
   // Contact
   contactPhone: '07 81 25 11 25',
